6bfc3ad57cf5e5fba2c1b08b7236d6c991e03098
westsai <westsai0411@gmail.com>
2016-05-01 22:13:07 -0700

    Use default hostapd
    need to verify

--- version2/AP_Setup/toRouter.sh
+++ version2/AP_Setup/toRouter.sh
@@ -17,9 +17,9 @@ sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
 sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
 # sudo cp ./interfaces /etc/network/interfaces	### already done ###
 # update hostapd
-sudo mv /usr/sbin/hostapd /usr/sbin/hostapd.ORIG
-sudo cp /home/pi/Pi_Setup/AP_Setup/hostapd /usr/sbin/
-sudo chmod 755 /usr/sbin/hostapd
+#sudo mv /usr/sbin/hostapd /usr/sbin/hostapd.ORIG
+#sudo cp /home/pi/Pi_Setup/AP_Setup/hostapd /usr/sbin/
+#sudo chmod 755 /usr/sbin/hostapd
 # set program as 'daemon'
 sudo service hostapd start
 sudo service isc-dhcp-server start

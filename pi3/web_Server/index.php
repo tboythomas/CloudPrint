97f5f0551bd1ebe95cb0c794a7e295fc89d91073
westsai <westsai@github.com>
2016-05-09 06:39:55 +0000

    verified working

--- pi3/web_Server/index.php
+++ pi3/web_Server/index.php
@@ -1,8 +1,9 @@
 <?php
-	$verified = shell_exec("sudo /home/pi/Pi_Setup/verify.sh");
+    $verified = shell_exec("sudo /home/pi/Pi_Setup/verify.sh");
+    print($verified);
     if($verified == 0) {
     	header('Location: errorPage.php'); 
     }else{
     	header('Location: verified.html');
-	}
+    }
 ?>

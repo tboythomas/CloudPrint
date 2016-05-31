2e9983e19215adf003cc3d3f316a565e7dbd2ee1
westsai <westsai0411@gmail.com>
2016-05-31 00:55:31 -0700

    Update init.ph to include removeOnePrinter.sh

--- pi3/init.py
+++ pi3/init.py
@@ -33,6 +33,7 @@ def main():
 	os.system("sudo chmod 744 /home/pi/Pi_Setup/cloudprint_Setup/connector.sh")
 	os.system("sudo chmod 744 /home/pi/Pi_Setup/cloudprint_Setup/printFail.sh")
 	os.system("sudo chmod 744 /home/pi/Pi_Setup/cloudprint_Setup/print.sh")
+	os.system("sudo chmod 744 /home/pi/Pi_Setup/cloudprint_Setup/removeOnePrinter.sh")
 	os.system("sudo chmod 744 /home/pi/Pi_Setup/removePrinters.sh")
 	os.system("sudo chmod 744 /home/pi/Pi_Setup/verify.sh")
 	# edit rc.local

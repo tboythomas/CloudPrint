0786fd7c7040ac70d0cc3f02a49ac692e83943a6
westsai <westsai0411@gmail.com>
2016-06-02 14:28:23 -0700

    Update connector.sh

--- version2/cloudprint_Setup/connector.sh
+++ version2/cloudprint_Setup/connector.sh
@@ -13,5 +13,8 @@ sudo bash /home/pi/Pi_Setup/cloudprint_Setup/cloudPrint.sh > /home/pi/Pi_Setup/c
 sleep 10
 sudo tail -n 1 /home/pi/Pi_Setup/cloudprint_Setup/log.txt > /home/pi/Pi_Setup/cloudprint_Setup/code.txt
 sudo cat /home/pi/Pi_Setup/cloudprint_Setup/success.txt /home/pi/Pi_Setup/cloudprint_Setup/code.txt > /home/pi/Pi_Setup/cloudprint_Setup/success_code.txt
-brand=`lpinfo -v | grep -E "direct usb" | sed -r -e "s/direct usb:\/\/([^/]*)\/.*/\1/"`
-sudo lp -d $brand /home/pi/Pi_Setup/cloudprint_Setup/success_code.txt
+usb=$(lpinfo -v | grep -E "direct usb" | head -1)
+model=$(echo $usb | sed -r -e "s/direct usb:\/\/([^/]*)\/([^%]*).*/\2/")
+brand=$(echo $usb | sed -r -e "s/direct usb:\/\/([^/]*)\/.*/\1/")
+name=${brand}_${model}
+sudo lp -d $name /home/pi/Pi_Setup/cloudprint_Setup/success_code.txt

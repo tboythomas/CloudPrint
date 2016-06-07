477c2ce43b7697b1b194742c4cb51c550e96eb5f
westsai <westsai0411@gmail.com>
2016-06-07 14:30:09 -0700

    Update addPrinter.sh

--- version2/cloudprint_Setup/addPrinter.sh
+++ version2/cloudprint_Setup/addPrinter.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
+<<FILE_DETAILS
+	File name: addPrinter.sh
+	Usage: addPrinter.sh
+	Description: This script detects any printers that are connected to
+		the USB port and identifies that ones that are not added to
+		the system. The scripts then add the un-added printers. It
+		also store the printers that are added in each execution in
+		the file /home/pi/Pi_Setup/cloudprint_Setup/lastPrinter.txt
+FILE_DETAILS
+
 if [ -f /home/pi/Pi_Setup/cloudprint_Setup/lastPrinter.txt ]; then
     sudo rm /home/pi/Pi_Setup/cloudprint_Setup/lastPrinter.txt
 fi
 sudo touch /home/pi/Pi_Setup/cloudprint_Setup/lastPrinter.txt
+#detects all usb printers
 usbs=$(lpinfo -v | grep -E "direct usb")
+#go through each usb printer
 while read -r usb
 do
+    #extract printer model name
     model=$(echo $usb | sed -r -e "s/direct usb:\/\/([^/]*)\/([^%]*).*/\2/")
     added=$(lpstat -p | grep "$model" | wc -l)
     if [ $added -eq 0 ]; then
@@ -20,7 +33,9 @@ do
         fi
 	name=${brand}_${model}
         echo "$name" >> /home/pi/Pi_Setup/cloudprint_Setup/lastPrinter.txt
+        #add the printer to the system
         lpadmin -p $name -E -m $driver -v $location
+        #print out a test page
         sudo lp -d $name /home/pi/Pi_Setup/cloudprint_Setup/testPrint.txt
 	sleep 5
     fi

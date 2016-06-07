df841595b59e2dd9687c8651492d2cd342d51cb2
westsai <westsai0411@gmail.com>
2016-06-07 14:40:08 -0700

    Update removePrinters.sh

--- version2/removePrinters.sh
+++ version2/removePrinters.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+<<FILE_DETAILS
+	File name: removePrinters.sh
+	Usage: removePrinters.sh
+	Description: This script removed all printers that have been added to
+		the system.
+FILE_DETAILS
 
+#list out all printers by name in printers.txt
 lpstat -p | grep printer | sed -r -e "s/printer ([^ ]*) .*/\1/" > printers.txt
+#removing all printers
 while read printer; do
 	lpadmin -x $printer
 done < printers.txt

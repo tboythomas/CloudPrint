c1e189a162182a8d775de9730c193f0480bbc2d2
westsai <westsai0411@gmail.com>
2016-06-07 14:35:52 -0700

    Update removeOnePrinter.sh

--- version2/cloudprint_Setup/removeOnePrinter.sh
+++ version2/cloudprint_Setup/removeOnePrinter.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+<<FILE_DETAILS
+	File name: removeOnePrinter.sh
+	Usage: removeOnePrinter.sh brand
+	Description: This script is used to remove only one printer at a time that 
+		failed to be added during auto printer set-up. This script uses the 
+		input arguement brand  and checks the lastPrinter file to find existing 
+		matching printer brand. Then it attempts to remove the printer and report 
+		back the result.
+FILE_DETAILS
 
+#look for printer that failed to be added
 printer=$(sudo cat /home/pi/Pi_Setup/cloudprint_Setup/lastPrinter.txt | grep -i $1)
+#remove the printer
 lpadmin -x $printer
 found=$(echo "$printer" | wc -l)
+#report back the result
 if [ -z "$printer" ]; then
 	echo "1"
 else

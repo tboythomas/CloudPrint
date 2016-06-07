039bac527adb8c0594af7514588a3bea901fa98f
westsai <westsai0411@gmail.com>
2016-06-07 15:28:16 -0700

    Update errorPage.php

--- version2/web_Server/errorPage.php
+++ version2/web_Server/errorPage.php
@@ -1,5 +1,5 @@
 <?php
-	# if the verification part fails, display this information
+	# if the verification fails, display this information
 	print("<h1> Warning: </h1>");
 	print("<p>We have detected that you are not using an official Plugable Google Cloud Print Server.
 			If this incorrect please visit http://plugable.com/support as we'd be more than happy to assist you.</p>");

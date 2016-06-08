5086c482ae5c64cb188c3aa91c961c22c32bd508
westsai <westsai0411@gmail.com>
2016-06-07 21:06:16 -0700

    Update errorPage.php

--- version2/web_Server/errorPage.php
+++ version2/web_Server/errorPage.php
@@ -1,7 +1,9 @@
 <?php
 '''
+FILE_DETAILS
     Description: If the verification page fails, display this page. It displays
     	the error message for unoffical device.
+FILE_DETAILS
 '''
 	print("<h1> Warning: </h1>");
 	print("<p>We have detected that you are not using an official Plugable Google Cloud Print Server.

920ba7d71632157de975a006dc4a9d074a259c3d
Thomas Cheng <toncheng15@hotmail.com>
2016-06-08 02:28:44 -0700

    fix the php comment
    fix the comment on errorPage.php and index.php

--- version2/web_Server/errorPage.php
+++ version2/web_Server/errorPage.php
@@ -1,10 +1,6 @@
 <?php
-'''
-FILE_DETAILS
-    Description: If the verification page fails, display this page. It displays
-    	the error message for unoffical device.
-FILE_DETAILS
-'''
+#If the verification page fails, display this page. It displays the error message for unoffical device.
+
 	print("<h1> Warning: </h1>");
 	print("<p>We have detected that you are not using an official Plugable Google Cloud Print Server.
 			If this incorrect please visit http://plugable.com/support as we'd be more than happy to assist you.</p>");
--- version2/web_Server/index.php
+++ version2/web_Server/index.php
@@ -1,10 +1,5 @@
 <?php
-'''
-FILE_DETAILS
-    Description: This page tests if this device is a valid Plugable product
-	and directs to verified.html if it is. Otherwise, to errorPage.html
-FILE_DETAILS
-'''
+# This page tests if this device is a valid Plugable product and directs to verified.html if it is. Otherwise, to errorPage.html
     $verified = shell_exec("sudo /home/pi/Pi_Setup/cloudprint_Setup/verify.sh");
     if($verified == 0) {
     	header('Location: errorPage.php'); 

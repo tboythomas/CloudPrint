7acc694d1dcfa9f2206a15b645519b512e97a153
westsai <westsai0411@gmail.com>
2016-06-07 20:51:57 -0700

    Update checkWifi.sh

--- version2/client_Setup/checkWifi.sh
+++ version2/client_Setup/checkWifi.sh
@@ -2,9 +2,9 @@
 <<FILE_DETAILS
     File name: checkWifi.sh
     Usage: checkWifi.sh WifiName
-    Description: This script checks if the Raspberry Pi has connected to the WiFi or not.
-    If the argument WiFiName is in the iwconfig wlan0 port list, outpus 1.
-    Otherwise, outputs 0.
+    Description: This script checks if the Raspberry Pi has connected to the given
+        WiFi or not. If the argument WiFiName is in the iwconfig wlan0 port list, 
+        outputs 1. Otherwise, outputs 0.
 FILE_DETAILS
 
 sudo iwconfig wlan0 | grep $1 | wc -l
